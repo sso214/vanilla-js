f7e37a634759e009beed5d5a80db441c9ba86f6a
sso214 <sojam214@gmail.com>
2023-02-21 21:36:24 +0900

    refactor : element 생성까지 class 내부에서 해결하도록 변경

--- stopwatch/custom.js
+++ stopwatch/custom.js
@@ -1,52 +1,82 @@
 class StopWatch {
-    #$timeBox;
-    #$buttonWrap;
-    #$recordList;
+    $timeBox;
+    $buttonWrap;
+    $recordList;
 
-    #time;
-    #timeout;
+    time;
+    timeout;
 
-    //엘리먼트 생성도 class 내부에서
-
-    constructor({ timeBox, buttonWrap, recordList }) {
-        this.#$timeBox = timeBox;
-        this.#$buttonWrap = buttonWrap;
-        this.#$recordList = recordList;
-
+    constructor() {
         this.#init();
     }
 
     get #timeStr() {
-        const timeFormat = this.#time.map((v) => `${v}`.padStart(2, '0'));
+        const timeFormat = this.time.map((v) => `${v}`.padStart(2, '0'));
         return timeFormat.join(':');
     }
 
     #getButton(type) {
-        return [...this.#$buttonWrap.childNodes].find((v) => v.dataset?.type === type);
+        return [...this.$buttonWrap.childNodes].find((v) => v.dataset?.type === type);
+    }
+
+    #getButtonName(type) {
+        return `${type[0].toUpperCase()}${type.substring(1,)}`;
     }
 
     #changeButton(before, after) {
         const target = this.#getButton(before);
         if (target) {
             target.dataset.type = after;
-            target.innerText = `${after[0].toUpperCase()}${after.substring(1,)}`;
+            target.innerText = this.#getButtonName(after);
         }
     }
 
     #setTime(time) {
-        this.#time = [...time];
+        this.time = [...time];
         this.#render();
     }
 
+    #initRender() {
+        const section = document.createElement('section');
+        const container = document.createElement('div');
+        this.$timeBox = document.createElement('p' )
+        this.$buttonWrap = document.createElement('div');
+        this.$recordList = document.createElement('ol');
+
+        section.style.cssText = 'max-width:500px;margin:100px auto;padding:20px;display:flex;align-items:center;border:1px solid #999;border-radius:5px;text-align:center';
+        container.style.cssText = 'flex:1';
+
+        this.$timeBox.style.cssText = 'margin-bottom:50px;font-size:30px;font-weight:bold';
+        this.$buttonWrap.style.cssText = 'display:flex;align-items:center;justify-content:space-between';
+        this.$recordList.style.cssText = 'margin-left:20px;flex:none;min-width:100px;height:300px;padding:10px;overflow:auto;border:1px solid #999;border-radius:5px;list-style:none';
+
+        ['start', 'reset', 'record'].forEach((key) => {
+            const button = document.createElement('button');
+            button.dataset.type = key;
+            button.style.cssText = 'height:45px;margin:3px 1%;display:inline-flex;align-items:center;justify-content:center;flex:1;color:#fff;background:#000;border:0;border-radius:3px;cursor:pointer';
+            button.innerText = this.#getButtonName(key);
+            this.$buttonWrap.appendChild(button);
+        });
+
+        this.$timeBox.innerText = '00:00:00';
+
+        container.appendChild(this.$timeBox);
+        container.appendChild(this.$buttonWrap);
+        section.appendChild(container);
+        section.appendChild(this.$recordList);
+        document.getElementById('app').appendChild(section);
+    }
+
     #init() {
+        this.#initRender();
         this.#setTime([0,0,0]);
         this.#addEvent();
     }
 
     #start() {
-        let [mn, sc, ms] = this.#time;
+        let [mn, sc, ms] = this.time;
 
-        this.#timeout = setInterval(() => {
+        this.timeout = setInterval(() => {
             if (ms === 99) {
                 ms = -1;
                 sc++;
@@ -64,25 +94,25 @@ class StopWatch {
     }
 
     #stop() {
-        clearTimeout(this.#timeout);
+        clearTimeout(this.timeout);
     }
 
     #reset() {
         this.#stop();
         this.#setTime([0,0,0]);
-        this.#$recordList.innerHTML = '';
+        this.$recordList.innerHTML = '';
     }
 
     #record() {
-        this.#$recordList.insertAdjacentHTML('afterbegin',`<li>${this.#timeStr}</li>`);
+        this.$recordList.insertAdjacentHTML('afterbegin',`<li>${this.#timeStr}</li>`);
     }
 
     #render() {
-        this.#$timeBox.innerHTML = this.#timeStr;
+        this.$timeBox.innerHTML = this.#timeStr;
     }
 
     #addEvent() {
-        this.#$buttonWrap.addEventListener('click', (e) => {
+        this.$buttonWrap.addEventListener('click', (e) => {
             const type = e.target.dataset.type;
 
             switch (type) {
@@ -108,8 +138,4 @@ class StopWatch {
     }
 }
 
-new StopWatch({
-    timeBox: document.getElementById('timeBox'),
-    buttonWrap: document.getElementById('buttonWrap'),
-    recordList: document.getElementById('recordList'),
-});
+new StopWatch();
